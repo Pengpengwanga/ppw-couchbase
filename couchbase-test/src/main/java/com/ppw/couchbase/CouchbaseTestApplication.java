c62323d0912f7b91ef777f5bc906b39f3b7a6c5f
Pengpeng Wang <pengpengw@vmware.com>
2019-05-08 15:20:59 +0800

    init project

---
+++ couchbase-test/src/main/java/com/ppw/couchbase/CouchbaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.ppw.couchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchbaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchbaseTestApplication.class, args);
+	}
+
+}
---
+++ couchbase-test/src/test/java/com/ppw/couchbase/CouchbaseTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ppw.couchbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CouchbaseTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
